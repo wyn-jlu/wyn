e820f076c0fb1c3004113b44a1a2be251873f5d2
wyn <yinliao1123232@foxmail.com>
2023-09-24 21:40:14 +0800

    进程调度注释添加

--- wyn/Linux/linux-0.11/include/linux/sys.h
+++ wyn/Linux/linux-0.11/include/linux/sys.h
@@ -70,7 +70,7 @@ extern int sys_sgetmask();
 extern int sys_ssetmask();
 extern int sys_setreuid();
 extern int sys_setregid();
-
+/* 各种函数指针组成的数组 */
 fn_ptr sys_call_table[] = { sys_setup, sys_exit, sys_fork, sys_read,
 sys_write, sys_open, sys_close, sys_waitpid, sys_creat, sys_link,
 sys_unlink, sys_execve, sys_chdir, sys_time, sys_mknod, sys_chmod,
